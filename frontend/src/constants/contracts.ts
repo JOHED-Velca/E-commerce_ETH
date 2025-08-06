04fa5461fe4db0bcb9e72f5be31e9370832175f5
Jonathan Velasquez <johedvelca@gmail.com>
2025-08-05 22:25:12 -0400

    Contracts deployed, working on useContractHook

---
+++ frontend/src/constants/contracts.ts
@@ -0,0 +1,5 @@
+import FlowCartTokenABI from './FlowCartToken.json';
+
+export const FLOWCART_TOKEN_ADDRESS = "0x446E97914c358ECB1063B27174f43423Cc460931";
+
+export const FLOWCART_TOKEN_ABI = FlowCartTokenABI;
