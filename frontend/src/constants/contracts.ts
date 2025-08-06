3ee7e2b58d456b3fa2221776a51ee6a61fde2163
Jonathan Velasquez <johedvelca@gmail.com>
2025-08-06 18:28:13 -0400

    PaymentGateway smart-contract deployed and added to hook

--- frontend/src/constants/contracts.ts
+++ frontend/src/constants/contracts.ts
@@ -1,5 +1,8 @@
 import FlowCartTokenJson from './FlowCartToken.json';
+import PaymentGatewayJson from './PaymentGateway.json';
 
 export const FLOWCART_TOKEN_ADDRESS = "0x446E97914c358ECB1063B27174f43423Cc460931";
-
 export const FLOWCART_TOKEN_ABI = FlowCartTokenJson.abi;
+
+export const PAYMENT_GATEWAY_ADDRESS = "0xFa7760517dA21821634A27969Ca77440812bFD19";
+export const PAYMENT_GATEWAY_ABI = PaymentGatewayJson.abi;
